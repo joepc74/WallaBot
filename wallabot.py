329d3d32a925c3fc0c6a77cf3896e3d80f36c3ce
Joe Colino <joepc74@gmail.com>
2024-02-19 16:56:50 +0100

    linea de comandos -1 par aun solo ciclo comentarios

--- wallabot.py
+++ wallabot.py
@@ -5,7 +5,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
 import config as cf
-import os,pickle,telebot
+import os,pickle,telebot, sys
 
 tb = telebot.TeleBot(cf.telegram_key)
 
@@ -33,24 +33,28 @@ def procesa_pagina(driver,entrada):
     driver.get(entrada['url'])
 
     try:
-        # sleep(10)
-        # saltar_terms_button = driver.find_element(By.LINK_TEXT,"Saltar")
-        # saltar_terms_button.click()
+        #esperamos que carguen los items de la web
         WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.CLASS_NAME,'ItemCardList__item')))
 
+        # buscamos todos los productos
         cards = driver.find_elements(By.CLASS_NAME,"ItemCardList__item")
         # print(cards)
         for card in cards:
             enlace=card.get_attribute('href')
             try:
+                # si esta en la lista previa no se hace nada
                 procesados.index(enlace)
             except:
+                # si NO esta en la lista previa
                 titulo=card.get_attribute('title')
                 imagen=card.find_element(By.TAG_NAME, "img").get_attribute('src')
                 precio=card.find_element(By.CLASS_NAME, "ItemCard__price").text
+                # enviamos mensaje por telegram
                 tb.send_message(cf.telegram_userid,f'<b>TITULO:</b> {titulo}\n<b>PRECIO:</b> {precio}\n<b>ENLACE:</b> {enlace}\n\n<b>IMAGEN:</b> {imagen}',parse_mode='HTML')
+                #añadimos el enlace
                 procesados.append(enlace)
                 haynuevos=True
+        # si ha habiado algun enlace guardamos la lista
         if (haynuevos):
             with open('procesados.pkl', 'wb') as fp:
                 pickle.dump(procesados, fp)
@@ -60,12 +64,22 @@ def procesa_pagina(driver,entrada):
 def main():
     options = webdriver.ChromeOptions()
     # options.add_argument('--headless')
-    while(True):
+    loop=True
+    while(loop):
         driver = webdriver.Chrome(options=options)
+        # preproceso de la web
         login(driver)
+        # procesamos los enlaces
         for enlace in cf.OFFERS:
             procesa_pagina(driver, enlace)
         driver.quit()
+        #si hay parametro 1 en linea de comandos se sale
+        try:
+            sys.argv.index('-1')
+            break
+        except:
+            pass
+        # esperar 30 segundos hasta siguiente ciclo
         sleep(30)
 
 
