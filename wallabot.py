7f14cdb09aa15731e4bd55b508bb41b126440558
Joe Colino <joepc74@gmail.com>
2025-11-10 13:55:20 +0100

    mejora en el procesamiento de páginas: actualiza selectores CSS y añade filtrado por título

--- wallabot.py
+++ wallabot.py
@@ -7,6 +7,7 @@ from time import sleep
 import config as cf
 import os,pickle,telebot, sys
 
+# inicializamos telegram bot
 tb = telebot.TeleBot(cf.telegram_key)
 
 # cargamos la lista de urls procesadas
@@ -39,11 +40,11 @@ def procesa_pagina(driver,entrada):
 
     try:
         #esperamos que carguen los items de la web
-        WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.CLASS_NAME,'ItemCardList__item')))
-
+        WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.CSS_SELECTOR,"a[class*='item-card']")))
         # buscamos todos los productos
-        cards = driver.find_elements(By.CLASS_NAME,"ItemCardList__item")
+        cards = driver.find_elements(By.CSS_SELECTOR,"a[class*='item-card']")
         # print(cards)
+        # input("Pulsa una tecla para continuar...")
         for card in cards:
             enlace=card.get_attribute('href')
             try:
@@ -52,8 +53,12 @@ def procesa_pagina(driver,entrada):
             except:
                 # si NO esta en la lista previa
                 titulo=card.get_attribute('title')
+                if ('filtrotitulo' in entrada):
+                    if titulo.lower().find(entrada['filtrotitulo'].lower())==-1:
+                        # si no cumple el filtro de titulo se ignora
+                        continue
                 imagen=card.find_element(By.TAG_NAME, "img").get_attribute('src')
-                precio=card.find_element(By.CLASS_NAME, "ItemCard__price").text
+                precio=card.find_element(By.CSS_SELECTOR, "strong[class*='Card__price']").text
                 if precio=='':
                     precio=card.find_element(By.CLASS_NAME, "item-detail-price_ItemDetailPrice--standard__TxPXr").text
                 # enviamos mensaje por telegram
@@ -69,7 +74,8 @@ def procesa_pagina(driver,entrada):
         if (haynuevos):
             with open('procesados.pkl', 'wb') as fp:
                 pickle.dump(procesados, fp)
-    except:
+    except Exception as e:
+        print(f"Error al procesar la pagina: {e}")
         pass
 
 def main():
@@ -79,6 +85,7 @@ def main():
         sys.argv.index('-nh')
     except:
         options.add_argument('--headless=new')
+        options.add_argument("--window-position=-2400,-2400")
     loop=True
     while(loop):
         print('Comenzando escaneo...')
